f95a5a80f77596c6313eb62848745ce9509a0a87
sidduvashista <sidduvashista000@gmail.com>
2022-08-19 10:33:14 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function SHOP(){
+    alert("add some items to cart")
+}
+function ADD(){
+    alert("added to cart")
+}
+var header =document.querySelector("a")
+header.style.color='blue'
+function getRandomColor()
+{
+    var letters ="0123456789ABCDEF";
+    var color="#";
+    for(var i=0;i<6;i++)
+    {
+        color +=letters[Math.floor(Math.random()*16)];
+    }
+    return color
+}
+function changeHeaderColor()
+{
+    colorInput =getRandomColor()
+    header.style.color = colorInput;
+}
+setInterval("changeHeaderColor()",500);
